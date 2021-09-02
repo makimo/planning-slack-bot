328df5832d75c901f1a6f50dd304889fb0ef21fc
Marcin Struś <marcin.strus@makimo.pl>
2021-09-02 15:07:56 +0200

    Deploy bot job to Nomad cluster

---
+++ terraform/bot.tf
@@ -0,0 +1,16 @@
+resource "nomad_job" "planning_bot" {
+  jobspec = templatefile(
+    "${path.module}/jobs/bot.nomad",
+    {
+      datacenters = [var.datacenter],
+      image       = "${aws_ecr_repository.planning_bot.repository_url}:${data.consul_keys.bot_image.var.tag}"
+    }
+  )
+}
+
+data "consul_keys" "bot_image" {
+  key {
+    name = "tag"
+    path = "internal/planning-bot/backend_tag"
+  }
+}
