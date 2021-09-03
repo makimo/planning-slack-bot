25b5695c9988d592511ac2379e0afec24c33f20d
Marcin Struś <marcin.strus@makimo.pl>
2021-09-03 14:18:16 +0200

    Bind SSL certificate for Planning Bot to ALB

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -13,3 +13,13 @@ resource "aws_route53_record" "planning_bot" {
     evaluate_target_health = true
   }
 }
+
+data "aws_lb_listener" "https" {
+  load_balancer_arn = data.aws_lb.main.arn
+  port              = 443
+}
+
+resource "aws_lb_listener_certificate" "backend" {
+  listener_arn    = data.aws_lb_listener.https.arn
+  certificate_arn = aws_acm_certificate.planning_bot.arn
+}
